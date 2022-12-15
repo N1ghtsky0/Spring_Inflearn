de9b71900d0fbf36692660c5b8ef26ca09e8e61d
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2022-12-15 16:54:04 +0900

    멤버 컨트롤러 클래스 이름 수정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MemeberController {
+public class MemberController {
     private final MemberService memberService;
 
     @Autowired
-    public MemeberController(MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 }
